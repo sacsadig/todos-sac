5212b5f4d86bb811759198755dd54f337dd5b4b7
Elsadig Adam <sadigatwork@outlook.com>
2025-05-30 23:48:43 +0200

    أضفت زر حذف لكل مهمة

--- js/app.js
+++ js/app.js
@@ -4,8 +4,12 @@ function addTask() {
   if (input.value.trim()) {
 
   const li = document.createElement("li");
-  li.className = "bg-grey-100 p-2 rounded mb-2";
-  li.textContent = input.value.trim();
+
+  li.className = "bg-gray-100 p-2 rounded mb-2 flex justify-between";
+  li.innerHTML = `
+      ${taskInput.value}
+      <button onclick="this.parentElement.remove()" class="text-red-500">✕</button>
+    `;
   taskList.appendChild(li);
   input.value = "";
 }
